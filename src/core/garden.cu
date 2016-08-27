299569f8408e3ac17ae99a3f3a58e1d79345e167
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2016-08-28 00:07:32 +0200

    stream processing

--- src/core/garden.cu
+++ src/core/garden.cu
@@ -124,27 +124,53 @@ namespace arboretum {
       std::vector<Split> _bestSplit;
 
       void FindBestSplits(const int level, const io::DataMatrix *data, const thrust::host_vector<float> &grad){
+        cudaStream_t s1, s2;
+        cudaStreamCreate(&s1);
+        cudaStreamCreate(&s2);
 
                       device_vector<unsigned int> row2Node = _rowIndex2Node;
                       device_vector<unsigned int> segments(data->rows);
                       device_vector<float> grad_sorted(data->rows);
                       device_vector<float> fvalue(data->rows + 1);
                       fvalue[0] = -std::numeric_limits<float>::infinity();
+                      device_vector<int> position(data->rows);
 
                       for(size_t fid = 0; fid < data->columns; ++fid){
 
-                          thrust::copy(data->sorted_data[fid].begin(), data->sorted_data[fid].end(), fvalue.begin() + 1);
-                          device_vector<int> position(data->index[fid].begin(), data->index[fid].end());
 
-                          thrust::gather(position.begin(),
+
+                          thrust::copy(thrust::cuda::par.on(s1),
+                                       data->sorted_data[fid].begin(),
+                                       data->sorted_data[fid].end(),
+                                       fvalue.begin() + 1);
+                          thrust::copy(thrust::cuda::par.on(s2),
+                                       data->index[fid].begin(),
+                                       data->index[fid].end(),
+                                       position.begin());
+
+                          // synchronize with both streams
+                          cudaStreamSynchronize(s1);
+                          cudaStreamSynchronize(s2);
+                          // destroy streams
+
+
+
+                          thrust::gather(thrust::cuda::par.on(s1),
+                                         position.begin(),
                                          position.end(),
                                          row2Node.begin(),
                                          segments.begin());
 
-                          thrust::gather(position.begin(),
+                          thrust::gather(thrust::cuda::par.on(s2),
+                                         position.begin(),
                                          position.end(),
                                          data->grad_device.begin(),
                                          grad_sorted.begin());
+
+                          // synchronize with both streams
+                          cudaStreamSynchronize(s1);
+                          cudaStreamSynchronize(s2);
+                          // destroy streams
 
                           thrust::stable_sort_by_key(segments.begin(),
                                                      segments.end(),
@@ -159,15 +185,21 @@ namespace arboretum {
 
                           thrust::equal_to<unsigned int> binary_pred;
 
-                          thrust::exclusive_scan_by_key(segments.begin(),
+                          thrust::exclusive_scan_by_key(thrust::cuda::par.on(s1),
+                                                        segments.begin(),
                                                         segments.end(),
                                                         grad_sorted.begin(),
                                                         sum.begin());
 
-                          thrust::exclusive_scan_by_key(segments.begin(),
+                          thrust::exclusive_scan_by_key(thrust::cuda::par.on(s2),
+                                                        segments.begin(),
                                                         segments.end(),
                                                         count.begin(),
                                                         count.begin());
+
+                          // synchronize with both streams
+                          cudaStreamSynchronize(s1);
+                          cudaStreamSynchronize(s2);
 
                           size_t lenght = 1 << level;
 
@@ -188,15 +220,21 @@ namespace arboretum {
                           device_vector<double> parent_node_sum_vector(data->rows, 0.0);
                           device_vector<size_t> parent_node_count_vector(data->rows, 0);
 
-                          thrust::gather(segments.begin(),
+                          thrust::gather(thrust::cuda::par.on(s1),
+                                         segments.begin(),
                                          segments.end(),
                                          parent_node_sum.begin(),
                                          parent_node_sum_vector.begin());
 
-                          thrust::gather(segments.begin(),
+                          thrust::gather(thrust::cuda::par.on(s2),
+                                         segments.begin(),
                                          segments.end(),
                                          parent_node_count.begin(),
                                          parent_node_count_vector.begin());
+
+                          // synchronize with both streams
+                          cudaStreamSynchronize(s1);
+                          cudaStreamSynchronize(s2);
 
                           thrust::for_each(
                                 thrust::make_zip_iterator(
@@ -270,6 +308,8 @@ namespace arboretum {
                             }
 
                         }
+                      cudaStreamDestroy(s1);
+                      cudaStreamDestroy(s2);
 
       }
       void UpdateNodeStat(const int level, const thrust::host_vector<float> &grad, const RegTree *tree){
