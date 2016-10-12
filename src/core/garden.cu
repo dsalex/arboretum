6c3d0695063911263236c5f2731137b238309978
Vladimir Ovsyannikov <vladimir.ovsyannikov@gmail.com>
2016-10-12 22:15:38 +0200

    no message

--- src/core/garden.cu
+++ src/core/garden.cu
@@ -38,9 +38,10 @@ namespace arboretum {
       for (size_t i = blockDim.x * blockIdx.x + threadIdx.x;
                i < n;
                i += gridDim.x * blockDim.x){
-          const int p = cub::ThreadLoad<cub::LOAD_LDG>(position + i);
-          out1[i] = cub::ThreadLoad<cub::LOAD_LDG>(in1 + p);
-          out2[i] = cub::ThreadLoad<cub::LOAD_LDG>(in2 + p);
+          const int p = cub::ThreadLoad<cub::LOAD_CS>(position + i);
+
+          cub::ThreadStore<cub::STORE_CS>(out1 + i, cub::ThreadLoad<cub::LOAD_CS>(in1 + p));
+          cub::ThreadStore<cub::STORE_CS>(out2 + i, cub::ThreadLoad<cub::LOAD_CS>(in2 + p));
         }
     }
 
